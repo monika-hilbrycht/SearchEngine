4e0267e7f8cfa80e6e6397508d97b0b3e9e1fbb0
Monika Hilbrycht <monika.hilbrycht@gmail.com>
2022-12-01 13:21:32 +0100

    Correction on script

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const searchEngine = () => {
     const text = e.target.value.toLowerCase();
 
     li.forEach(el => {
-        const task = el.textContent;
-
-        if (task.toLowerCase().indexOf(text) !== -1) {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
             el.style.display = 'block'
         } else {
             el.style.display = 'none'
